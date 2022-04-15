a40dbef28ca9a054110731524a2789820063a9a2
Rodgers Kipchumba <rodgers@Rodgers-MacBook-Pro.local>
2022-04-15 16:11:20 +0300

    First commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '856',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lavender Drone',
+        productNumber: '124',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Drone',
+        productNumber: '631',
+        paymentStatus: 'Paid',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'DJI Air 25',
+        productNumber: '861',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lozenge Drone',
+        productNumber: '112',
+        paymentStatus: 'Paid',
+        shipping: 'Deliverd'
+    },
+]
